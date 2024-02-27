659b389c298173b2b531b0d8f99b7b7e2e892104
gonzagaa <iigust4vo@gmail.com>
2024-02-27 17:14:05 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+    showButtonWhatsappOnScroll()
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+}
+
+function showButtonWhatsappOnScroll() {
+    if(scrollY > 200) {
+        document.querySelector("#buttonWhatsapp").classList.add("show")
+    } else {
+        document.querySelector("#buttonWhatsapp").classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+  });
+
